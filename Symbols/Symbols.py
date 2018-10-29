9f84c0d321373d6b3f15dd22ee0276b4c58aa693
Obehi <stianoi@ifi.uio.no>
2018-10-29 09:59:29 +0100

    small refactoring code

--- Symbols/Symbols.py
+++ Symbols/Symbols.py
@@ -1,17 +1,6 @@
 class Term:
     def __init__(self, value):
         self.value = float(value)
-        print('creating term - type:{}   value:{}'.format(type(self.value), self.value))
-    def  __add__(self, other):
-        return self.value + other
-    def  __sub__(self, other):
-        return self.value - other
-
-    def  __mul__(self, other):
-        return self.value * other
-
-    def  __floordiv__(self, other):
-        return self.value / other
 
     def  __repr__(self):
         return '{}'.format(self.value)
@@ -24,18 +13,14 @@ class Expression:
     def __init__(self, *args):
         #convert the tuple to list
         
-        print("creating Expression args: {} ".format(args))
-        print("type(args): {}".format(type(args)))
         self.value = args[0]
-        print("making a list of args into value: {} ".format(self.value))
-        print("type(self.value): {}".format(type(self.value)))
 
     def get(self):
         return self.value
 
     def get_first(self):
         if not self.value:
-            return EnvironmentError
+            raise Exception('Expression: List of symbols is empty')
         return self.value[0]
 
     def length(self):
@@ -44,9 +29,9 @@ class Expression:
     # replace index and sorrounding elements with value
     def replace(self, index, value):
         if index % 2 == 0:
-            return EnvironmentError
+            raise Exception('Expression: Operator is in wrong index')
         if index >= len(self.value):
-            return EnvironmentError
+            raise Exception('Expression: Operator is out of bound')
 
         term_one = self.value[index-1]
         term_two = self.value[index+1]
@@ -64,9 +49,6 @@ class Operator:
     def __init__(self, value):
         self.value = value
 
-    def __eq__(self, other):
-         return self.value
-         
     def  __repr__(self):
         return '{}'.format(self.value)
 
--- engine.py
+++ engine.py
@@ -11,39 +11,10 @@ def make_tokens(string):
     remove_space = re.compile(r'\s+')
     no_space_string = re.sub(remove_space, '', string)
     
+    # Splits on word, digit and parenthesis, but not words and digits withn parenthesis
     token_list = [i for i in re.split(r'(\(.*?\)|\d+|.)', no_space_string) if i]
     return token_list
 
-def check_syntax(symbol_list):
-    even_symbols = symbol_list[::2]
-    odd_symbols = symbol_list[1::2]
-
-    # Check for empy expressions
-    if not symbol_list:
-        abort(400, description="Empty Expression.")
-        
-    
-    # Check for expressions that are of even length
-    if len(symbol_list) % 2 == 0:
-        abort(400, description="Expression is of an even length.")
-        
-
-    # Check for wronly placed Operators
-    if any(isinstance(x, Symbols.Operator) for x in even_symbols):
-        abort(400, description="Wrong Operator position.")
-        
-    
-    # Check for wronly placed terms or expressions
-    if any(isinstance(symbol, Symbols.Term) or isinstance(symbol, Symbols.Expression) for symbol in odd_symbols):
-        abort(400, description="Wrong Term og Expression position.")
-        
-    
-    # check parantheses expressions for syntax error too
-    expression = next((symbol for symbol in symbol_list if isinstance(symbol, Symbols.Expression)), False)
-    if expression:
-        return check_syntax(expression.get())
-
-    return True
 
 # Make a list of Symbols from tokens
 def make_symbols(token_list):
@@ -78,6 +49,8 @@ def make_symbols(token_list):
         t = t+1
     return Symbols.Expression(symbol_list)
 
+
+
 def make_expression(s):
     # Strip "()" from string
     new_expression_string = s[1:-1]
@@ -85,7 +58,59 @@ def make_expression(s):
     new_symbol_list = make_symbols(expression_token_list)
     return new_symbol_list
 
+def isDigit(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
 
+def isOperator(s):
+    if any(x in s for x in ('/', '*', '-', '+')):
+        return True
+    else:
+        return False
+ 
+def is_expression(s):
+    if s.startswith('('):
+        return True
+    else:
+        return False
+
+#Using the even-odd structure of mathematical expressions to check syntax
+def check_syntax(symbol_list):
+    even_symbols = symbol_list[::2]
+    odd_symbols = symbol_list[1::2]
+
+    # Check for empy expressions
+    if not symbol_list:
+        abort(400, description="Empty Expression.")
+        
+    
+    # Check for expressions that are of even length
+    if len(symbol_list) % 2 == 0:
+        abort(400, description="Expression is of an even length.")
+        
+
+    # Check for wronly placed Operators
+    if any(isinstance(x, Symbols.Operator) for x in even_symbols):
+        abort(400, description="Wrong Operator position.")
+        
+    
+    # Check for wronly placed terms or expressions
+    if any(isinstance(symbol, Symbols.Term) or isinstance(symbol, Symbols.Expression) for symbol in odd_symbols):
+        abort(400, description="Wrong Term og Expression position.")
+        
+    
+    # check parantheses expressions for syntax error too
+    expression = next((symbol for symbol in symbol_list if isinstance(symbol, Symbols.Expression)), False)
+    if expression:
+        return check_syntax(expression.get())
+
+    return True
+    
+
+# Returns the index of the operator with the highest precedence
 def get_precedence_index(expression):
     # The order in which to process operators
     operator_precedence = ['/','*','+','-']
@@ -94,8 +119,7 @@ def get_precedence_index(expression):
         index = next((s for s, item in enumerate(expression.get()) if item.value == i), False)
         if index:
             return index
-    return EnvironmentError
-
+    abort(501, description="Internal error: get_precedence_index() cant find next operator.")
 
 def evaluate_expression(expression):
     if expression.length() > 3:
@@ -121,58 +145,34 @@ def evaluate_operator(t1, op, t2):
         sum = evaluate_symbol(t1) / evaluate_symbol(t2)
     return Symbols.Term(sum) 
 
-# Takes a symbol and returns a Term
+# Takes a a singel symbol and returns a Term
 def evaluate_symbol(symbol):
     if isinstance(symbol, Symbols.Term):
         return symbol.value
     if isinstance(symbol, Symbols.Expression):
+        # If the symbol is an expression, evaluate the expression before evaluation of the symbol
         return evaluate_symbol(evaluate_expression(symbol))
    
 
-def isDigit(s):
-    try: 
-        int(s)
-        return True
-    except ValueError:
-        return False
 
-def isOperator(s):
-    if any(x in s for x in ('/', '*', '-', '+')):
-        return True
-    else:
-        return False
- 
-def is_expression(s):
-    if s.startswith('('):
-        return True
-    else:
-        return False
 
+# Entry point for engine
 def calc_json_expression(json_expression):
     tokens = make_tokens(json_expression)
     symbols_sequence = make_symbols(tokens)
-    if not check_syntax(symbols_sequence.get()):
-        pass
-        #raise SyntaxError("test it")
-        #return "syntax error", 404
-    print('symbols_sequence')
-    print(symbols_sequence)
-    print(symbols_sequence)
-    print(symbols_sequence)
+    check_syntax(symbols_sequence.get())
+      
+   
     extra_list = []
     extra_list.append(symbols_sequence)
-    print(extra_list)
-    print(extra_list)
+    
     result_str = evaluate_expression(symbols_sequence).value
     result = { "result" : result_str}
     add_history_item(json_expression, result_str)
     return result
 
 def add_history_item(expression, result):
-    item = {}
-    item['expression'] = expression
-    item['result'] = result
-    history.append(item)
+    history.append({'expression': result})
 
 def get_history():
     return history
